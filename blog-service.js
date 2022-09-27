a0d9cfa1af35ff8f821c261d12729926a652d503
Ching Wei Lai <HTY140226@gmail.com>
2022-09-27 18:39:28 -0500

    modify the file

--- blog-service.js
+++ blog-service.js
@@ -8,6 +8,53 @@
 *  Online (Cyclic) Link: ________________________________________________________
 *
 ********************************************************************************/
+const fs = require("fs");
 
+let posts = [];
 
+module.exports.initialize = function() {
+    return new Promise ((resolve,reject) => {
+        fs.readFile('./data/posts.json'),(err,data) => {
+            if(err){
+                reject("unable to read file");
+            }
+            else {
+                posts = JSON.parse(data)
+                resolve();
+            }
+        }
+    })
+}
 
+module.exports.getAllPosts = function() {
+    return new Promise((resolve,reject) => {
+        if (posts.length == 0) {
+            reject("File is empty, no posts to be displayed.");
+        }
+        else {
+            resolve(posts);
+        }
+    })
+}
+
+module.exports.getPublishedPosts = function() {
+    return new Promise((resolve,reject) => {
+        if (posts.length == 0) {
+            reject("File is empty, no posts to be displayed.");
+        }
+        else {
+            resolve(posts.published == true)
+        }
+    })
+}
+
+module.exports.getCategories = function() {
+    return new Promise((resolve,reject) => {
+        if(categories.length == 0) {
+            reject("File is empty, no categories to be displayed.");
+        }
+        else {
+            resolve (categories);
+        }
+    })
+}
