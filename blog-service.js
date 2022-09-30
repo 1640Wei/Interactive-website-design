ecc5283c2950ba139c133a121a219aee498c392d
Ching Wei Lai <HTY140226@gmail.com>
2022-09-29 23:34:57 -0500

    modify the file

--- blog-service.js
+++ blog-service.js
@@ -3,9 +3,9 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Ching Wei Lai   Student ID: 136893211   Date: 30 Sep 2022
+*  Name: Ching Wei Lai   Student ID: 136893211   Date: 29 Sep 2022
 *
-*  Online (Cyclic) Link: ________________________________________________________
+*  Online (Cyclic) Link: https://better-calf-scarf.cyclic.app
 *
 ********************************************************************************/
 const fs = require("fs");
