69ba31af1fedf528172d1020241564b40290c1c6
Ching Wei Lai <HTY140226@gmail.com>
2022-11-02 19:00:56 -0500

    modify the file

--- blog-service.js
+++ blog-service.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Ching Wei Lai   Student ID: 136893211   Date: 14 Oct 2022
+*  Name: Ching Wei Lai   Student ID: 136893211   Date: 4 Nov 2022
 *
-*  Online (Cyclic) Link: https://better-calf-scarf.cyclic.app
+*  Online (Cyclic) Link: 
 *
 ********************************************************************************/
 const fs = require("fs");
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /*********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Ching Wei Lai   Student ID: 136893211   Date: 14 Oct 2022
+*  Name: Ching Wei Lai   Student ID: 136893211   Date: 4 Nov 2022
 *
-*  Online (Cyclic) Link: https://better-calf-scarf.cyclic.app
+*  Online (Cyclic) Link: 
 *
 ********************************************************************************/
 
@@ -16,11 +16,34 @@ const data = require("./blog-service.js");
 const multer = require("multer");
 const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
+const exphbs = require('express-handlebars');
 
 var app = express();
 app.use(express.static('public')); 
 app.use(express.urlencoded({ extended: true })); 
 const upload = multer();
+
+app.engine('.hbs', exphbs.engine({
+    extname: '.hbs', 
+    helpers: {
+        navLink: function(url, options){
+            return '<li' + 
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+        },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }        
+    }
+}));
+app.set('view engine', '.hbs');
+
+
 
 var HTTP_PORT = process.env.PORT || 8080;
 function onHTTPSTART() {
@@ -31,9 +54,17 @@ app.get("/", function (req, res) {
     res.redirect("/about");
 });
 
-app.get("/about", function (req, res) {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
-});
+// app.get("/about", (req, res) => {
+// 	res.render("about");
+// });
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});  
+app.get('/posts/add', (req, res) => {
+    res.render("addPost")
+})
+
 
 app.get("/blog", function(req,res) {
     res.sendFile(path.join(__dirname,"data/posts.json"));
@@ -43,9 +74,9 @@ app.get("/categories", function(req,res) {
     res.sendFile(path.join(__dirname,"data/categories.json"));
 });
 
-app.get("/posts/add", function (req, res) {
-    res.sendFile(path.join(__dirname, "/views/addPost.html"));
- });
+// app.get("/posts/add", function (req, res) {
+//     res.sendFile(path.join(__dirname, "/views/addPost.html"));
+//  });
 
 app.get("/posts", function (req, res) {
     if(req.query.category) {
@@ -146,4 +177,13 @@ req.body.featureImage = imageUrl;
 blogService.addPost(req.body).then(() => {
 res.redirect("/posts");
 })
+});
+
+
+
+app.use(function(req,res,next){
+    let route = req.path.substring(1);
+    app.locals.activeRoute = "/" + (isNaN(route.split('/')[1]) ? route.replace(/\/(?!.*)/, "") : route.replace(/\/(.*)/, ""));
+    app.locals.viewingCategory = req.query.category;
+    next();
 });
